15857d8276d10f84eafbb2b8a7313dc0b9e31079
lihaoming <1436928794@qq.com>
2023-10-22 22:28:06 +0800

    添加了内容

--- git/6.远程库的操作.js
+++ git/6.远程库的操作.js
@@ -1,6 +1,12 @@
 /*
+    git config --global http.proxy http://127.0.0.1:33210
+    git config --global https.proxy http://127.0.0.1:33210
+    export https_proxy=http://127.0.0.1:33210 http_proxy=http://127.0.0.1:33210 all_proxy=socks5://127.0.0.1:33211
     远程库操作的命令
         1. git remote ：查看当前关联的远程库
-        2. git remote add 新建库名 url地址 ：关联远程仓库
-        3. 
+        2. git remote -v ：查看当前关联的远程库的详细信息
+        3. git remote add 新建库名 url地址 ：关联远程仓库
+        4. git remote remove 库名 ：删除关联的远程库
+        5. git push 远程库名 分支名 ：向远程仓库推送代码
+        6. git push -u 远程库名 分支名 ：向远程仓库推送代码，并产生关联
 */
